ef8c7fec2f851c90582f596ec22385b5bcdb919e
Naman Anand <nanand@twilio.com>
2022-07-22 16:59:28 +0530

    init

---
+++ app/src/main/java/com/voidmemories/weatherapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.voidmemories.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
